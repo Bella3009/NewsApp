e13a1068615e1698c725af1924da41379854049d
Bella3009 <120841781+Bella3009@users.noreply.github.com>
2023-02-17 21:13:46 +0100

    Update main.py
    Updated with links, made dynamic topic and language

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import requests
+#from send_email import send_email
 
 #newsapi.org
 api_key = "d16abece8b5446b698fbbab95c443604"
 topic = input("What topic you wish to see: ")
 lang = input("Choose language: en or it ")
-url = f"https://newsapi.org/v2/everything?q={topic}&from=2023-01-16&sortBy=publishedAt&apiKey={api_key}&language={lang}"
+url = f"https://newsapi.org/v2/everything?q={topic}&from=2023-02-16&sortBy=publishedAt&apiKey={api_key}&language={lang}"
 
 r = requests.get(url)
 content = r.json()
+body = ""
 
-for article in content['articles']:
-    print(article['title'])
-    print(article['description'])
+for article in content['articles'][:20]:
+    if article["title"] is not None:
+        body = body + article['title'] + "\n" + article['description'] + + "\n" + article['url'] (2*"\n")
+    
+body = body.encode("utf-8")
+#send_email(body)
