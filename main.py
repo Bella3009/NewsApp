8c18929c5f0eacc4943e32f6ec1467369c7d983c
Bella3009 <bellasara13@gmail.com>
2023-05-28 16:31:07 +0200

    Sending the news result to the email.

--- main.py
+++ main.py
@@ -1,19 +1,28 @@
 import requests
-#from send_email import send_email
+from send_email import send_email
 
 #newsapi.org
 api_key = "d16abece8b5446b698fbbab95c443604"
 topic = input("What topic you wish to see: ")
 lang = input("Choose language: en or it ")
-url = f"https://newsapi.org/v2/everything?q={topic}&from=2023-02-16&sortBy=publishedAt&apiKey={api_key}&language={lang}"
+url = f"https://newsapi.org/v2/everything?q={topic}&sortBy=publishedAt&apiKey={api_key}&language={lang}"
 
+# Making the request
 r = requests.get(url)
 content = r.json()
 body = ""
 
+# Retrieve the data
 for article in content['articles'][:20]:
     if article["title"] is not None:
-        body = body + article['title'] + "\n" + article['description'] + + "\n" + article['url'] (2*"\n")
-    
-body = body.encode("utf-8")
-#send_email(body)
+        body = body + article['title'] + "\n" + article['description'] + "\n" + article['url'] + 2*"\n"
+
+# Add also a subject to the email.
+message = f"""\
+Subject: News titles
+
+{body}
+"""
+
+message = message.encode("utf-8")
+send_email(message)
